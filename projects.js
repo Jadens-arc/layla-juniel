1ca753515dc88ee36f2c332a6fb8e33c9d0d0c95
Jaden Arceneaux <arceneauxjaden@gmail.com>
2023-10-14 12:12:06 -0700

    init

---
+++ projects.js
@@ -0,0 +1,13 @@
+let projects = [
+  {
+    "title": "Trip to Sri Lanka",
+    "body": "I had the fortune to travel to Sri Lanka for 2 months to study elephant-human conflicts.",
+  },
+  {
+    "title": "Vibrant Voices",
+    "body": "A podcast I cohost that speaks on the experiences of UNR's most underrepresented groups.",
+    "links": [
+      "https://www.vibrantvoicesunr.com/",
+    ]
+  },
+];
